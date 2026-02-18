3e356b673c3977f6372a1651caa482b4d3fe7859
Reyzikuuu <virmanadipuraaaa@gmail.com>
2026-02-18 23:14:47 +0700

    chore: enable automated image compression (41% size reduction)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   // config options
   base: './', // Ensures relative paths for GitHub Pages / Subfolders
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 75,
+      },
+      webp: {
+        quality: 80,
+        lossless: false,
+      },
+    }),
+  ],
 })
